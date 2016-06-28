db309d15ef1298357ee7f180ad82cba816fc354c
Tanguy P <tanguy.penet@gmail.com>
2016-06-29 00:29:26 +0200

    Client side: add call to Amazon API and display results

--- web/finderReactApp.js
+++ web/finderReactApp.js
@@ -1,3 +1,60 @@
+/**
+ * Represents one result of the Amazon Search API
+ */
+var AmazonSearchResult = React.createClass({
+	
+	render: function() {
+		return React.createElement(
+			'div',
+			null,
+			React.createElement('h2', null, this.props.ItemAttributes.Title),
+			React.createElement('a', {href: this.props.DetailPageURL}, 'See details')
+		);
+	}
+	
+});
+
+/**
+ * List of results of the Amazon Search API
+ */
+var ListAmazonSearchResults = React.createClass({
+	
+	render: function() {
+		if (this.props.results !== null) {
+			var results = this.props.results;
+			
+			// No results, or there was a problem in the Amazon API call
+			if (results.metaApiStatus !== 0 || !results.apiResponse || !results.apiResponse.Items.Item) {
+				var msg;
+				if (results.metaApiStatus !== 0) {
+					msg = results.metaApiMessage || 'An unknown error occurred during the call to the Amazon API (sorry, that\'s vague but we have no additional information)';
+				}
+				else {
+					msg = "No results were found";
+				}
+				
+				return React.createElement('div', {className: 'search-api-message'}, msg);
+			}
+			
+			// Found results: display them
+			return React.createElement(
+				'div',
+				null,
+				results.apiResponse.Items.Item.map(function(item, index) {
+					return React.createElement(AmazonSearchResult, item);
+				})
+			);
+		}
+		
+		// Nothing to display yet (initial state)
+		return React.createElement('div', null);
+	}
+	
+});
+
+/**
+ * Represents one result of the Rent Search API
+ */
 var RentSearchResult = React.createClass({
 	
 	render: function() {
@@ -21,7 +78,7 @@ var RentSearchResult = React.createClass({
  * but, for better scalability, 2 distinct React components have been created for (1) the list
  * of results and for (2) each individual result
  */
-var RentSearchResultWrapper = React.createClass({
+var ListRentSearchResults = React.createClass({
 	
 	render: function() {
 		if (this.props.results) {
@@ -30,7 +87,7 @@ var RentSearchResultWrapper = React.createClass({
 			// No results for search, or malformed API response
 			if (! (results.name && results.price)) {
 				var msg = results.msg || 'An error occurred while searching';
-				return React.createElement('div', {className: 'rent-search-api-message'}, msg);
+				return React.createElement('div', {className: 'search-api-message'}, msg);
 			}
 			
 			// Found results: display them
@@ -57,14 +114,36 @@ var SearchForm = React.createClass({
 	searchItem: function(evt) {
 		evt.preventDefault();
 		var item = $('#searchedItemInput')[0].value;
+		
+		// Call Rent Search API
+		var fullRentApiUrl = this.props.rentApiUrl + item;
 		$.ajax({
-			url: this.props.url + item,
+			url: fullRentApiUrl,
 			success: function(data) {
 				// Trigger refresh from StuffFinder
 				this.props.updateRentSearchResults(data);
 			}.bind(this),
 			error: function(xhr, status, err) {
-				console.error(this.props.url, status, err.toString());
+				console.error(fullRentApiUrl, status, err.toString());
+			}.bind(this)
+		});
+		
+		// Call Amazon Meta-API (extra layer of security)
+		var fullAmazonApiUrl = globalConfig.metaApiServer + "/meta_api/ajax.php";
+		$.ajax({
+			url: fullAmazonApiUrl,
+			data: {
+				password: globalConfig.metaApiPassword,
+				api: 'amazon',
+				SearchIndex: 'All',
+				Keywords: encodeURIComponent(item)
+			},
+			success: function(data) {
+				// Trigger refresh from StuffFinder
+				this.props.updateAmazonSearchResults(data);
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(fullAmazonApiUrl, status, err.toString());
 			}.bind(this)
 		});
 	},
@@ -87,20 +166,51 @@ var StuffFinder = React.createClass({
 	
 	getInitialState: function() {
 		return {
-			rentSearchResults: null
+			rentSearchResults: null,
+			amazonSearchResults: null
 		};
 	},
 	
+	/**
+	 * Triggers an update of the ListRentSearchResults component
+	 * @param {object} rentSearchResults	The new search results
+	 */
 	updateRentSearchResults: function(rentSearchResults) {
 		this.setState({rentSearchResults: rentSearchResults});
+	},
+	
+	/**
+	 * Triggers an update of the ListAmazonSearchResults component
+	 * @param {object} amazonSearchResults	The new search results
+	 */
+	updateAmazonSearchResults: function(amazonSearchResults) {
+		this.setState({amazonSearchResults: amazonSearchResults});
 	},
 	
 	render: function() {
 		return React.createElement(
 			'div',
 			null,
-			React.createElement(SearchForm, {url: '/search/', updateRentSearchResults: this.updateRentSearchResults.bind(this)}),
-			React.createElement(RentSearchResultWrapper, {results: this.state.rentSearchResults})
+			React.createElement(
+				SearchForm,
+				{
+					rentApiUrl: '/search/',
+					updateRentSearchResults: this.updateRentSearchResults.bind(this),
+					updateAmazonSearchResults: this.updateAmazonSearchResults.bind(this)
+				}
+			),
+			React.createElement(
+				ListRentSearchResults,
+				{
+					results: this.state.rentSearchResults
+				}
+			),
+			React.createElement(
+				ListAmazonSearchResults,
+				{
+					results: this.state.amazonSearchResults
+				}
+			)
 		);
 	}
 });
